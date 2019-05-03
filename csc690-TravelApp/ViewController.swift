28afc18ea101bd7bc2a61e9a8461dc323dac5683
Siu <kungsiuchun@gmailcom>
2019-05-03 02:58:01 -0700

    create Tarvel App for csc690 milestone 2

---
+++ csc690-TravelApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  csc690-TravelApp
+//
+//  Created by SiuChun Kung on 5/2/19.
+//  Copyright © 2019 SiuChun Kung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
